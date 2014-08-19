9eff041f30c7b8fba4cd3d0d464ecc2f7c8d637e
unknown <unaor@unaor7-l1.corp.TFHNET.LOCAL>
2014-08-19 14:51:07 -0500

    enabled cors

--- src/main/java/com/datasol/cuponza/controller/WelcomeController.java
+++ src/main/java/com/datasol/cuponza/controller/WelcomeController.java
@@ -1,5 +1,11 @@
 package com.datasol.cuponza.controller;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +14,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 	@RequestMapping(value="/",method = RequestMethod.GET)
-	public @ResponseBody String index(){
+	public @ResponseBody String index(HttpServletRequest request, HttpServletResponse response){
+//		  HttpHeaders responseHeaders = new HttpHeaders();	   
+//		  Enumeration headerNames = request.getHeaderNames();
+//		  while(headerNames.hasMoreElements()){
+//		   String nextElement = (String)headerNames.nextElement();
+//		   System.out.println(nextElement + "=" + request.getHeaders(nextElement));
+//		   responseHeaders.set(nextElement, request.getHeader(nextElement));
+//		  }
+		   
+		   
+		  //populating the header required for CORS
+		 // responseHeaders.set("Access-Control-Allow-Origin", "*"); 
 		return "hello";
 	}
 
